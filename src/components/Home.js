58ebfce7fe221c583440f605e577cf2d55fef3c3
Sudhanshu <ksudhanshu394@gmail.com>
2023-11-20 11:33:17 +0530

    adding all files

---
+++ src/components/Home.js
@@ -0,0 +1,195 @@
+import React, { useState, useEffect, useRef } from "react";
+import myStyle from "./css/Home.module.css";
+import menu from "./img/menu.png";
+import profile from "./img/profile.jpg";
+import { v4 as uuidv4 } from 'uuid';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { allActions } from "../state";
+import { bindActionCreators } from "redux";
+
+
+export default function Home() {
+  const [display, setDisplay] = useState(false);
+  const [value, setValue] = useState("");
+  const [suggesstion, setSuggesstion] = useState([]);
+  const [action, setAction] = useState(false);
+  const ref = useRef(null);
+  // code for using redux for setting the serah input data
+  const dispatch = useDispatch();
+  const { setSearchData } = bindActionCreators(allActions, dispatch);
+
+  const navigate = useNavigate();
+
+  // code for useeffect
+
+  useEffect(() => {
+    const firstTimeSearch = async () => {
+      let apiKey = ``; // enter your api for serach suggestion
+      let fetchData = await fetch(apiKey);
+      let convertData = await fetchData.json();
+      setSuggesstion(convertData);
+      setAction(true);
+    };
+    firstTimeSearch();
+  }, [value]);
+
+  // code for set data to input fiel by suggestion method
+  const setOptionVal = (data) => {
+    setValue(data);
+    ref.current.focus();
+  };
+
+  // code for function for toogle them
+  const toogleIcon = () => {
+    setDisplay(!display);
+  };
+  // function for handel enter submit
+  const handelKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      setSearchData(value);
+      navigate("/search");
+    }
+  };
+
+  return (
+    <>
+      {/* CODE FOR HEADER SECTION */}
+      <div className="container-fluid mt-3">
+        <div className="row">
+          <div className="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-4 offset-xl-9 col-lg-9 offset-md-7 offset-sm-5 offset-5 d-flex justify-content-end">
+            <span className={myStyle.gmailImg}>Gmail</span>&nbsp; &nbsp;&nbsp;
+            <span className={myStyle.gmailImg}>Images</span>
+          </div>
+          <div className="col-xl-1 col-lg-1 col-md-2 col-sm-3 col-3 d-flex justify-content-center">
+            <img
+              src={menu}
+              alt="menu"
+              className={myStyle.menuDot}
+              onClick={toogleIcon}
+            />
+            <img src={profile} alt="profile" className={myStyle.profile} />
+          </div>
+        </div>
+      </div>
+
+      {/* CODE FOR MAIN CONTAINER OF UI */}
+      <div className={`container ${myStyle.mainContainerOfContent}`}>
+        <div className="row">
+          <div className="col-12 d-flex justify-content-center">
+            <h1 className={myStyle.googleText}>Google</h1>
+          </div>
+          <div className="col-12 d-flex justify-content-center mt-4">
+            <div className={myStyle.mainDivOfSerachIcon}>
+              <i
+                className={`fa-solid fa-magnifying-glass ${myStyle.glass}`}
+              ></i>
+              <input
+                type="text"
+                name="search"
+                id="search"
+                ref={ref}
+                value={value}
+                className={myStyle.searchInput}
+                placeholder="Search Google or type a URL"
+                onChange={(e) => {
+                  setValue(e.target.value);
+                }}
+                onKeyUp={handelKeyPress}
+                autoComplete="off"
+              />
+              <i className={`fa-solid fa-microphone ${myStyle.microphone}`}></i>
+            </div>
+          </div>
+          {/* div for suggestion showing */}
+
+          <div className={`${myStyle.mainDivOfSuggestion}`}>
+            {action
+              ? suggesstion[1].map((ele) => {
+                  return (
+                    <p
+                      className={`${myStyle.suggestOption}`}
+                      onClick={() => setOptionVal(ele)} key={uuidv4()}
+                    >
+                      <i
+                        className={`fa-solid fa-magnifying-glass ${myStyle.optionGlass}`}
+                      ></i>{" "}
+                      <span className={myStyle.optionText}>{ele}</span>
+                    </p>
+                  );
+                })
+              : ""}
+          </div>
+
+          {/* code for two btns */}
+          <div className="col-12 d-flex justify-content-center mt-xl-5 mt-md-4 mt-sm-3 mt-3">
+            <button type="button" className={myStyle.twoBtnOfHomePage}>
+              Google Search
+            </button>
+            <button type="button" className={myStyle.twoBtnOfHomePage}>
+              I'm Feeling Lucky
+            </button>
+          </div>
+
+          {/* code for gogole offered in  */}
+          <div className="col-12 d-flex justify-content-center mt-4">
+            <p className={myStyle.googleOfferedStyle}>
+              Google offered in :{" "}
+              <span className={myStyle.languageStyle}>Hindi</span>
+              <span className={myStyle.languageStyle}>English</span>
+              <span className={myStyle.languageStyle}>Marathi</span>
+              <span className={myStyle.languageStyle}>Punjabi</span>
+              <span className={myStyle.languageStyle}>Gujarati</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* code for showing all the apps */}
+
+      {display ? (
+        <div className="container-fluid">
+          <div className="row">
+            <div
+              className={`col-xl-3 col-lg-3 col-md-6 col-sm-10 col-10 ${myStyle.allAppOptionsStyle}`}
+            >
+              <div className="row">
+
+              <div className="col-3 d-flex justify-content-center">
+                <i className={`fa-brands fa-youtube ${myStyle.appIconStyle}`} style={{color: "#eb0f0f"}}></i>
+                </div>
+
+                <div className="col-3 d-flex justify-content-center">
+                  <i
+                    className={`fa-brands fa-google-plus-g ${myStyle.appIconStyle}`}
+                    style={{ color: "rgb(2, 2, 41); " }}
+                  ></i>
+                </div>
+
+               
+
+                <div className="col-3 d-flex justify-content-center">
+                  <i
+                    className={`fa-brands fa-google-drive ${myStyle.appIconStyle}`}
+                    style={{ color: "orangered" }}
+                  ></i>
+                </div>
+
+                <div className="col-3 d-flex justify-content-center">
+                  <i
+                    className={`fa-sharp fa-solid fa-location-dot ${myStyle.appIconStyle}`}
+                    style={{ color: "#0959e1" }}
+                  ></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <footer>
+        <i className="fa-solid fa-pen" style={{ color: "white" }}></i>
+      </footer>
+    </>
+  );
+}
---
+++ src/components/Search.js
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from "react";
+import SearchStyle from "./css/Search.module.css";
+import google from './img/google.png'
+import { v4 as uuidv4 } from 'uuid';
+import { useSelector } from "react-redux";
+import LoadingBar from "react-top-loading-bar";
+export default function Search() {
+  const searchFData = useSelector((state) => state.mySearchData);
+  const [searchInput, setSearchInput] = useState(searchFData);
+  const [option, setOption] = useState(searchInput);
+  const [dataFetchByApi, setDataFetchByAPi] = useState(false);
+  const [progress, setProgress] = useState(0);
+  // usestate for accessing result of data of api
+  const [getDataAPI, setDataAPI] = useState([]);
+  // code for api handling
+
+  useEffect(() => {
+    const firstTimeSearch = async () => {
+      setProgress(10);
+      let apiKey = ``; // enter your api for  search
+      setProgress(30);
+      let fetchData = await fetch(apiKey);
+      setProgress(60);
+      console.log(fetchData)
+      let convertData = await fetchData.json();
+      setProgress(90);
+      setDataAPI(convertData);
+      setProgress(100);
+      setDataFetchByAPi(true);
+    };
+    firstTimeSearch();
+  }, [option]);
+
+  // function for handel enter submit
+  const handelKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      setOption(searchInput);
+    }
+  };
+
+  return (
+    <>
+      {/* code for loading bar */}
+      <LoadingBar
+        color="#f11946"
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <div className="container-fluid mt-4">
+        <div className="row">
+          <div className="col-xl-2 col-lg-2 col-md-3 col-sm-0 col-0 d-flex justify-content-center">
+            <h2 className={SearchStyle.mobileResp}>Google</h2>
+          </div>
+
+          <div className="col-xl-5 col-lg-5 col-md-7 col-sm-12 col-12">
+            <div className={SearchStyle.mainDivOfSerachIcon}>
+              <i
+                className={`fa-solid fa-magnifying-glass ${SearchStyle.magnifyingGlass}`}
+              ></i>
+              <input
+                type="search"
+                name="search"
+                id="search"
+                className={SearchStyle.input}
+                placeholder="Search Google or type a URL"
+                autoComplete="off"
+                value={searchInput}
+                onChange={(e) => {
+                  setSearchInput(e.target.value);
+                }}
+                onKeyUp={handelKeyPress}
+              />
+
+              {/* code for options*/}
+            </div>
+            {dataFetchByApi ? (
+              <div className={`${SearchStyle.options} mt-4`}>
+                {getDataAPI.search_information.menu_items.map((element) => {
+                  return (
+                    <a
+                      key={element.position}
+                      href={element.link}
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{ textDecoration: "none" }}
+                    >
+                      <span className={SearchStyle.topOptions}>
+                        {element.title}
+                      </span>
+                    </a>
+                  );
+                })}
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </div>
+      {dataFetchByApi ? <hr /> : ""}
+      {dataFetchByApi ? (
+        <div className="container mb-5">
+          <p className={`${SearchStyle.searchResult} mb-5`}>
+            About {getDataAPI.search_information.total_results} results (
+            {getDataAPI.search_information.time_taken_displayed}){" "}
+          </p>
+          {getDataAPI.organic_results.map((ele) => {
+            return (
+              <div
+                className="row mx-xl-5 mx-lg-5 mx-md-2 mx-sm-0 mx-0 mt-3"
+                key={uuidv4()}
+              >
+                <div className="col-xl-8 col-lg-8 co-md-10 col-sm-12 col-12">
+                  <div style={{ display: "flex" }}>
+                    <img
+                      src={ele.favicon}
+                      alt="icon"
+                      className={SearchStyle.webIconStyle}
+                    />
+                    <div style={{ display: "grid" }}>
+                      <span className={SearchStyle.labelHead}>
+                        {ele.source}
+                      </span>
+                      <small
+                        className={SearchStyle.labelHead}
+                        style={{ fontSize: "11px" }}
+                      >
+                        {ele.displayed_link}{" "}
+                        <i
+                          className={`fa-solid fa-ellipsis-vertical ${SearchStyle.iconStyleThreeDots}`}
+                          style={{ marginLeft: "30px", fontSize: "15px" }}
+                        ></i>
+                      </small>
+                    </div>
+                  </div>
+                  <a href={ele.link} target="_blank" rel="noreferrer">
+                    <strong
+                      className="mt-3"
+                      style={{ color: "rgb(138,180,248)" }}
+                    >
+                      {ele.title}
+                    </strong>
+                  </a>
+                  <p style={{ fontWeight: "lighter" }}>{ele.snippet}</p>
+                </div>
+              </div>
+            );
+          })}
+
+          <div className="row mx-xl-5 mx-lg-5 mx-md-2 mx-sm-0 mx-0 mt-3">
+            <div className="col-12">
+              <img src={google} alt="logo" className={SearchStyle.googleLogo} />
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
